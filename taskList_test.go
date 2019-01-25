7e22a66d844504c67673b105fbec14e0a12c1833
Seth Lowie <slowie@powerchord.com>
2019-01-25 09:58:35 -0500

    app structure update

--- taskList_test.go
+++ taskList_test.go
@@ -10,4 +10,18 @@ func Test_TaskList(t *testing.T) {
 			t.Fail()
 		}
 	})
+
+	t.Run("add task", func(t *testing.T) {
+		tl := TaskList{}
+
+		newTask := Task{
+			title: "",
+		}
+
+		tl.Add(newTask)
+
+		if len(tl) != 1 {
+			t.Fail()
+		}
+	})
 }

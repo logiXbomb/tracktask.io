a2a4aa62b8b1f3427ebfce39c6b4e4c7acb88e75
Seth Lowie <slowie@powerchord.com>
2019-01-31 12:05:17 -0500

    remove dist

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
-	echo "step 1"
-	git config --global user.email $GH_EMAIL
-	git config --global user.name $GH_NAME
+	echo "step 1 [SET GIT USER]"
+	git config --global user.email "bot@tracktask.io"
+	git config --global user.name "Bot"
 
+	echo "step 2 [CLONE REPO]"
 	git clone $CIRCLE_REPOSITORY_URL out
 
 	cd out
 	git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 	git rm -rf .
-	cd ../repo
 
-	npm run build
+	cd ../
 
-	cd ..
+	# npm run build
 
-	mv ./repo/index.html ./out/index.html
-	mv ./repo/index.js ./out/index.js
-	mv ./repo/index.map ./out/index.map
-	mv ./repo/worker.js ./out/worker.js
-	mv ./repo/worker.map ./out/worker.map
-	mv ./repo/manifest.json ./out/manifest.json
-	mv ./repo/CNAME ./out/CNAME
+	ls -la
 
-	mkdir -p out/.circleci && cp -a repo/.circleci/. out/.circleci/.
-	cd out
+	# mv ./repo/index.html ./out/index.html
+	# mv ./repo/index.js ./out/index.js
+	# mv ./repo/index.map ./out/index.map
+	# mv ./repo/worker.js ./out/worker.js
+	# mv ./repo/worker.map ./out/worker.map
+	# mv ./repo/manifest.json ./out/manifest.json
+	# mv ./repo/CNAME ./out/CNAME
 
-	git add -A
-	git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+	# mkdir -p out/.circleci && cp -a repo/.circleci/. out/.circleci/.
+	# cd out
 
-	git push origin $TARGET_BRANCH
+	# git add -A
+	# git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+	# git push origin $TARGET_BRANCH
 fi

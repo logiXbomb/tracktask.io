cc99a94ac080f1ba77d53c4602ba567bc6c8cd5b
Seth Lowie <slowie@powerchord.com>
2019-01-31 12:26:49 -0500

    building to correct folder

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
-if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
-	echo "step 1 [SET GIT USER]"
-	git config --global user.email "bot@tracktask.io"
-	git config --global user.name "Bot"
+echo "step 1 [SET GIT USER]"
+git config --global user.email "bot@tracktask.io"
+git config --global user.name "Bot"
 
-	echo "step 2 [CLONE REPO]"
-	git clone $CIRCLE_REPOSITORY_URL out
+echo "step 2 [CLONE REPO]"
+git clone $CIRCLE_REPOSITORY_URL out
 
-	cd out
-	git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-	git rm -rf .
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git rm -rf .
 
-	cd ../
+cd ../
 
-	# npm run build
+npm run build
 
-	ls -la
+ls -la dist
 
-	# mv ./repo/index.html ./out/index.html
-	# mv ./repo/index.js ./out/index.js
-	# mv ./repo/index.map ./out/index.map
-	# mv ./repo/worker.js ./out/worker.js
-	# mv ./repo/worker.map ./out/worker.map
-	# mv ./repo/manifest.json ./out/manifest.json
-	# mv ./repo/CNAME ./out/CNAME
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
-	# mkdir -p out/.circleci && cp -a repo/.circleci/. out/.circleci/.
-	# cd out
+cp -v ./dist/* ./out
+cp ./CNAME ./out
 
-	# git add -A
-	# git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+ls -la out
 
-	# git push origin $TARGET_BRANCH
-fi
+# mkdir -p out/.circleci && cp -a repo/.circleci/. out/.circleci/.
+# cd out
+
+# git add -A
+# git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+# git push origin $TARGET_BRANCH
